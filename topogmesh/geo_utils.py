63ce598341677559393a4943e3a20ca7e6f04bdc
FinleyStirk06 <FinleyStirk06@gmail.com>
2025-08-19 15:02:20 +0100

    convert to latlon if not already

--- topogmesh/geo_utils.py
+++ topogmesh/geo_utils.py
@@ -11,6 +11,15 @@ def to_utm(input_raster: RasterLayer) -> RasterLayer:
     x_center = gt[0] + (ds.RasterXSize * gt[1]) / 2
     y_center = gt[3] + (ds.RasterYSize * gt[5]) / 2
 
+    src_srs = osr.SpatialReference()
+    src_srs.ImportFromWkt(ds.GetProjection())
+
+    if not src_srs.IsGeographic():
+        target_srs = osr.SpatialReference()
+        target_srs.ImportFromEPSG(4326)  # WGS84
+        ct = osr.CoordinateTransformation(src_srs, target_srs)
+        x_center, y_center, _ = ct.TransformPoint(x_center, y_center)
+
     utm_zone = int((x_center + 180) / 6) + 1
     hemisphere = 'north' if y_center >= 0 else 'south'
 
